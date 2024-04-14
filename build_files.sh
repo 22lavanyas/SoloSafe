7bda639485be07769b07b4ca6edabad1d63fa704
Nishant-Bharwani <nbtaylor1031@gmail.com>
2024-04-15 00:17:46 +0530

    build_files.sh updated

--- build_files.sh
+++ build_files.sh
@@ -1,2 +1,7 @@
-pip install -r requirements.txt 
-python3.9 manage.py collectstatic
+# pip install -r requirements.txt 
+# python3.9 manage.py collectstatic
+
+#!/bin/bash
+python3.9 -m pip install -r requirements.txt
+python3.9 manage.py collectstatic --noinput
+
