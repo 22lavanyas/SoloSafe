95dc0dd28da59066bcdb0bc52174590feeff0492
Nishant-Bharwani <nbtaylor1031@gmail.com>
2024-04-15 00:36:36 +0530

    Changes done

--- build_files.sh
+++ build_files.sh
@@ -4,7 +4,7 @@
 #!/bin/bash
 python3.9 -m ensurepip
 python3.9 -m pip install --upgrade pip
-.\venv\Scripts\activate
+source venv/bin/activate
 python3.9 -m pip install -r requirements.txt
 python3.9 manage.py collectstatic --noinput
 
