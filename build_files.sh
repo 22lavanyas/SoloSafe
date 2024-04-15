2c2dbe17114ab0aa2cee81bc944d95c35d06a38c
Nishant-Bharwani <nbtaylor1031@gmail.com>
2024-04-15 13:49:42 +0530

    Configs changed

--- build_files.sh
+++ build_files.sh
@@ -3,19 +3,19 @@
 
 #!/bin/bash
 
-echo "Pip installation/updation and virtual environment activation. . ."
+# echo "Pip installation/updation and virtual environment activation. . ."
 python3.9 -m ensurepip
 python3.9 -m pip install --upgrade pip
 python3.9 -m venv myenv
 source myenv/bin/activate
 
-echo "Installing requirements.txt. . ."
+# echo "Installing requirements.txt. . ."
 python3.9 -m pip install -r requirements.txt
 
-echo "Making migrations. . ."
+# echo "Making migrations. . ."
 python3.9 manage.py makemigrations --noinput
 python3.9 manage.py migrate --noinput
 
-echo "Collect static. . ."
+# echo "Collect static. . ."
 python3.9 manage.py collectstatic --noinput
 
