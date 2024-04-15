37926960b3528c1160c83d5ac8cd794d3c603700
Nishant-Bharwani <nbtaylor1031@gmail.com>
2024-04-15 15:48:55 +0530

    Changed

--- mysite/settings.py
+++ mysite/settings.py
@@ -29,7 +29,7 @@ OMEGA = BASE_DIR
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['https://solosafe.onrender.com', '.vercel.app','now.sh','127.0.0.1','localhost']
+ALLOWED_HOSTS = ['solosafe.onrender.com', '.vercel.app','now.sh','127.0.0.1','localhost']
 
 
 # Application definition
