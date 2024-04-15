84c5fe1c8f1a617e6f9d7ffa57d05061f89a74e6
Nishant-Bharwani <nbtaylor1031@gmail.com>
2024-04-15 12:58:54 +0530

    change

--- build_files.sh
+++ build_files.sh
@@ -2,10 +2,10 @@
 # python3.9 manage.py collectstatic
 
 #!/bin/bash
-python3.8 -m ensurepip
-python3.8 -m pip install --upgrade pip
-python3.8 -m venv myenv
+python3.9 -m ensurepip
+python3.9 -m pip install --upgrade pip
+python3.9 -m venv myenv
 source myenv/bin/activate
-python3.8 -m pip install -r requirements.txt
-python3.8 manage.py collectstatic --noinput
+python3.9 -m pip install -r requirements.txt
+python3.9 manage.py collectstatic --noinput
 
